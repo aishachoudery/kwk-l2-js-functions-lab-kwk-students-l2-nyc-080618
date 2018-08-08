be3064742e1db63643d5d6174b2f8d72c190a7d5
aishachoudery <aichoudery31@gmail.com>
2018-08-08 19:50:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks (someValue) {
+  return Math.abs(someValue-42);
+}
+
+function distanceFromHqInFeet (someValue) {
+  return distanceFromHqInBlocks(someValue) * 264;
+}
+
+function distanceTravelledInFeet(someValue1, somevalue2) {
+  return Math.abs(somevalue2 - someValue1) * 264;
+}
+
+function calculatesFarePrice(start, destination) {
+  var distance = destination - start
+  if distance <= 400
+    console.log("This ones on us!")
+  else if distance <= 2000 
+}
+
+  return (distanceTravelledInFeet - 400) * 2;
+  
